47d3229da283a621af24106fc27876d872f85361
vasanthaneppali <vassu1509@gmail.com>
2021-11-06 14:10:33 +0530

    changed again python file

--- hungry.py
+++ hungry.py
@@ -2,4 +2,7 @@ hungry=input("i am hungry")
 if hungry=="yes":
     print("eat fruites")
 else:
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+        print("drink water")
     print("do the work")
